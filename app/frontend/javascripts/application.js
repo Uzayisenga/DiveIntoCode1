4ff31f8ecf064d75b7055f9ab62273d46c211045
Amina <aminauzayisenga@gmail.com>
2019-11-15 17:47:44 +0200

    Make team leader have access to team

---
+++ app/frontend/javascripts/application.js
@@ -0,0 +1,6 @@
+import 'stylesheets/application';
+import 'bootstrap/dist/js/bootstrap.min.js'
+import 'admin-lte/dist/js/adminlte.min.js'
+import "path to application.css"
+import Rails from 'rails-ujs';
+Rails.start();
